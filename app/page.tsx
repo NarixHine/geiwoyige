4a7791119e373884a061f34d7a47fe71c08b9178
Narix Hine <127665924+NarixHine@users.noreply.github.com>
2024-09-15 16:21:53 +0800

    Fix typings

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,8 @@ import ClassroomDraw from './draw'
 import { cookies } from 'next/headers'
 
 export default function Home() {
-  const mockNames = [
-    "爱丽丝", "鲍勃", "查理", "戴安娜", "伊桑", "菲奥娜", "乔治", "汉娜",
-    "伊恩", "朱莉娅", "凯文", "连姆", "米娅", "诺亚", "奥利维亚", "保罗",
-    "奎因", "瑞秋", "山姆", "蒂娜", "尤玛", "维克多", "温蒂", "泽维尔",
-    "雅拉", "扎克", "亚当", "贝拉", "科迪", "黛西", "艾玛", "弗兰克",
-    "格蕾丝", "亨利", "艾薇", "杰克", "凯特", "里奥", "玛雅", "内特",
-    "奥斯卡", "佩妮", "昆西", "罗斯", "西蒙", "汤姆", "乌苏拉", "维奥莱特"
-  ]
   const cookieNames = cookies().get('availableNames')
   return (
-    <ClassroomDraw names={JSON.parse(cookieNames?.value || '[]') || mockNames} />
+    <ClassroomDraw names={JSON.parse(cookieNames?.value || '[]')} />
   )
 }
